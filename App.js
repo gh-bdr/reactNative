e488de7d8187cfa2f6b5b9dc553d96e49ed2fb2f
gh-bdr <66869258+gh-bdr@users.noreply.github.com>
2021-03-07 04:29:36 -0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! sdfqsfji myyyyy work now</Text>
       <StatusBar style="auto" />
     </View>
   );
